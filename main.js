93cc7452187c98c66ecb85f222deabc435942384
unknown <luuvythuan1204@gmail.com>
2024-08-29 21:22:43 +0700

    updatecode

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+const fs = require('fs')
 const http  = require('http')
-
+const url = require('url')
 const NewServer = http.createServer((req,res) => {
-      res.end('helo from server !')
+     const pathName = req.url
+
+     if ( pathName === '/' || pathName === '/oveview') {
+        res.end('This page overview !')
+     } else if (pathName === '/product') {
+        res.end('This is page product !')
+     } else if(pathName === '/api') {
+            fs.readFile(`${__dirname}/dev-data/data.json` ,'utf-8', (err ,data) => {
+                const ProductData = JSON.parse(data)
+                res.writeHead(200 , {'Content-type' : 'application/json'})
+                res.end(data)
+            })
+           
+     } else {
+    
+        res.end('Page not Found !')
+     }
 })
 
 NewServer.listen(3000 , (req,res) => {
